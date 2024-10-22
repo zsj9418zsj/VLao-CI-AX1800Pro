4137587b6317a99318a1d266e4e08e5ed11edffd
zsj9418 <34057614+zsj9418@users.noreply.github.com>
2024-10-22 23:27:20 +0800

    Update Feeds.sh

--- Scripts/Feeds.sh
+++ Scripts/Feeds.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # 追加源
-echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/aarch64_cortex-a53/kiddin9' >>feeds.conf.default
