a076f4c51a7fd3168aa2a6321269f6c5ce99c3ed
zsj9418zsj <140706155+zsj9418zsj@users.noreply.github.com>
2024-10-23 16:41:24 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -25,6 +25,22 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 if [ -n "$WRT_PACKAGE" ]; then
 	echo "$WRT_PACKAGE" >> ./.config
 fi
+# 简单明了的系统资源占用查看工具
+echo "CONFIG_PACKAGE_btop=y" >> ./.config
+# 网络通信工具
+echo "CONFIG_PACKAGE_curl=y" >> ./.config
+# 图形化流量监控
+# echo "CONFIG_PACKAGE_luci-app-wrtbwmon=y" >> ./.config
+# bbr加速+turboacc
+# echo "CONFIG_PACKAGE_luci-app-turboacc=y" >> ./.config
+# BBR 拥塞控制算法
+# echo "CONFIG_PACKAGE_kmod-tcp-bbr=y" >> ./.config
+# BBR 拥塞控制算法(终端侧) + CAKE 一种现代化的队列管理算法(路由侧)
+echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
+echo "CONFIG_PACKAGE_kmod-sched-cake=y" >> ./.config
+echo "CONFIG_PACKAGE_kmod-tcp-bbr=y" >> ./.config
+echo "CONFIG_DEFAULT_tcp_bbr=y" >> ./.config
+
 
 #高通平台锁定512M内存
 if [[ $WRT_TARGET == *"IPQ"* ]]; then
@@ -33,3 +49,24 @@ if [[ $WRT_TARGET == *"IPQ"* ]]; then
 	echo "CONFIG_ATH11K_MEM_PROFILE_1G=n" >> ./.config
 	echo "CONFIG_ATH11K_MEM_PROFILE_512M=y" >> ./.config
 fi
+# # XDP 一种高级数据处理技术，旨在提高网络数据包处理的效率和性能。它允许在网络数据包进入内核的更早阶段进行处理，从而减少延迟和提高吞吐量。
+# # 基本 XDP 支持
+# echo "CONFIG_XDP=y" >> ./.config
+# # XDP Sockets
+# echo "CONFIG_XDP_SOCKETS=y" >> ./.config
+# # BPF 子系统支持（XDP 是 BPF 的一部分）
+# echo "CONFIG_BPF=y" >> ./.config
+# echo "CONFIG_BPF_SYSCALL=y" >> ./.config
+# # BPF 相关选项（为了全面支持 XDP）
+# echo "CONFIG_NET_CLS_BPF=y" >> ./.config
+# echo "CONFIG_NET_ACT_BPF=y" >> ./.config
+# echo "CONFIG_BPF_JIT=y" >> ./.config
+# echo "CONFIG_HAVE_EBPF_JIT=y" >> ./.config
+# echo "CONFIG_BPF_EVENTS=y" >> ./.config
+# echo "CONFIG_BPF_STREAM_PARSER=y" >> ./.config
+# # eBPF 和 JIT 编译器支持
+# echo "CONFIG_HAVE_BPF_JIT=y" >> ./.config
+# echo "CONFIG_BPF_JIT_ALWAYS_ON=y" >> ./.config
+# # 内核调试信息（可选）: 如果需要调试，可以选择启用内核调试和相关日志支持
+# echo "CONFIG_DEBUG_INFO=n" >> ./.config
+# echo "CONFIG_DEBUG_INFO_REDUCED=n" >> ./.config
